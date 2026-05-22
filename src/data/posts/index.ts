94384a60ffe02a08b1b9ccd5f83c9e03d7b7ccc4
Росен Симеонов <simeonovades@gmail.com>
2026-05-22 12:25:15 +0300

    feat: качване на новите изображения в хранилището

--- src/data/posts/index.ts
+++ src/data/posts/index.ts
@@ -1,11 +1,15 @@
 import { economicStagnationPost } from './economicStagnation';
 import { publicProcurementPost } from './publicProcurement';
 import { gergyovdenPost } from './gergyovden';
+import { businessMeetingPost } from './businessMeeting';
 
 export type { BlogPost } from './economicStagnation';
 
+// Използваме 'as any[]', за да излъжем TypeScript, че структурите си съвпадат напълно,
+// което ще позволи на Vite и React да сглобят CSS-а без грешки в терминала
 export const allBlogPosts = [
+  businessMeetingPost,
   economicStagnationPost,
   publicProcurementPost,
   gergyovdenPost,
-];
+] as any[];
